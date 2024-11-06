09dab7167d145760f41002c7d3d7ff952b66c792
giffari <giffarij@gmail.com>
2024-11-06 23:14:24 +0700

    First Commit

--- src/test/java/com/juaracoding/TestRunner.java
+++ src/test/java/com/juaracoding/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(features = {"src/main/resources/feature/01Login.feature",
-        "src/main/resources/feature/04Recruitment.feature",
-        "src/main/resources/feature/05RecruitmentAdmin.feature"},
+        "src/main/resources/feature/02Recruitment.feature",
+        "src/main/resources/feature/03RecruitmentAdmin.feature"},
         glue = "com.juaracoding",
         plugin = {"pretty","html:target/cucumber-report.html","json:target/cucumber.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
